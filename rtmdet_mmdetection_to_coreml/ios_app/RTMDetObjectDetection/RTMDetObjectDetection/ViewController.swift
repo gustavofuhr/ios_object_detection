ab7c91ca31e574a34a328cf45cb7f7c6dfb54e6a
Gustavo Führ <gustavofuhr87@gmail.com>
2024-07-20 11:52:40 -0300

    FIX: some stuff

---
+++ rtmdet_mmdetection_to_coreml/ios_app/RTMDetObjectDetection/RTMDetObjectDetection/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  RTMDetObjectDetection
+//
+//  Created by Gustavo Führ on 19/07/24.
+//
+
+import UIKit
+
+var mlModel = try! rtmdet_tiny(configuration: .init()).model
+
+
+class ViewController: UIViewController {
+
+    private var videoCapture: VideoCapture!
+    
+    private let videoSize = CGSize(width: 1280, height: 720)
+    private let preferredFps: Int32 = 2
+
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var bbView: BoundingBoxView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let spec = VideoSpec(fps: preferredFps, size: videoSize)
+        let frameInterval = 1.0 / Double(preferredFps)
+        
+        videoCapture = VideoCapture(cameraType: .back,
+                                    preferredSpec: spec,
+                                    previewContainer: cameraView.layer)
+        videoCapture.imageBufferHandler = {[unowned self] (imageBuffer, timestamp, outputBuffer) in
+            let delay = CACurrentMediaTime() - timestamp.seconds
+            if delay > frameInterval {
+                return
+            }
+
+            self.serialQueue.async {
+                self.runModel(imageBuffer: imageBuffer)
+            }
+        }
+        
+        let modelPaths = Bundle.main.paths(forResourcesOfType: "mlmodel", inDirectory: "models")
+        
+        modelUrls = []
+        for modelPath in modelPaths {
+            let url = URL(fileURLWithPath: modelPath)
+            let compiledUrl = try! MLModel.compileModel(at: url)
+            modelUrls.append(compiledUrl)
+        }
+        
+        selectModel(url: modelUrls.first!)
+        
+        // scaleFill
+        cropAndScaleOptionSelector.selectedSegmentIndex = 2
+        updateCropAndScaleOption()
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.startCapture()
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.resizePreview()
+        // TODO: Should be dynamically determined
+        self.bbView.updateSize(for: CGSize(width: videoSize.height, height: videoSize.width))
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        guard let videoCapture = videoCapture else {return}
+        videoCapture.stopCapture()
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+
+
+}
+
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 from .plot import plot_detections
 from .image_proc import resize_keep_ratio
-from .postprocess import postprocess_mmdetection_output
 
 __all__ = [
-    'plot_detections', 'resize_keep_ratio', 'postprocess_mmdetection_output'
+    'plot_detections', 'resize_keep_ratio'
 ]
